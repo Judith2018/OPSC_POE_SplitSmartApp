602155e278919f608f4c8538c2a5c5cdbee3ec1b
Harris0109 <mumeenaharris69@gmail.com>
2025-10-05 11:11:09 +0200

    Added to colors.xml file and created styles.xml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Added dependencies
+//    implementation (androidx.appcompat:appcompat:1.6.1)
+//    implementation (com.google.android.material:material:1.9.0)
+//    implementation (androidx.constraintlayout:constraintlayout:2.1.4)
+//    implementation (androidx.recyclerview:recyclerview:1.3.0)
+//    implementation (androidx.cardview:cardview:1.0.0)
+//    implementation (androidx.core:core-ktx:1.10.1)
+//    implementation (androidx.lifecycle:lifecycle-runtime-ktx:2.6.1)
+//    implementation (androidx.activity:activity-ktx:1.8.0)
+
+    // Firebase placeholders (Role 1 will add actual versions & apply plugin)
+    // implementation 'com.google.firebase:firebase-auth:XX.X.X'
+    // implementation 'com.google.firebase:firebase-firestore:XX.X.X'
 }
+
