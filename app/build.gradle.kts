06cdadb64ce7ec697164a703a5d7f8c7c6a37deb
mulen <mulendajudith2018@gmail.com>
2025-10-14 21:20:33 +0200

    Added the API dependencies code in the build gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,10 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
     implementation("com.google.firebase:firebase-auth")
     //implementation 'com.google.firebase:firebase-firestore'
+
+    //API
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
 
 
